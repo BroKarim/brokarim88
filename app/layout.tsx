054877054b9975cb36d973bfa4bdbbedb4e89390
BroKarim <bukutulis0110@gmail.com>
2026-03-01 09:39:59 +0700

    refactor(layout): simplify BackgroundLoader component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import TVNoise from "@/components/tv-noise";
 import { ModeProvider } from "@/context/mode";
 import { ModeSwitcher } from "@/components/mode/mode-switcher";
 import { ModeWrapper } from "@/components/mode/mode-wrapper";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { BackgroundLoader } from "@/components/background-placeholder";
 
@@ -51,22 +52,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const BACKGROUND_PLACEHOLDER = "https://res.cloudinary.com/dctl5pihh/image/upload/w_20,q_30,e_blur:200/v1768287477/background_valoru.jpg";
-const BACKGROUND_OPTIMIZED = "https://res.cloudinary.com/dctl5pihh/image/upload/f_auto,q_auto,w_1920/v1768287477/background_valoru.jpg";
+  const BACKGROUND_OPTIMIZED = "https://res.cloudinary.com/dctl5pihh/image/upload/f_auto,q_auto,w_1920/v1768287477/background_valoru.jpg";
   return (
-    
     <html lang="en">
       <body className={` ${geistMono.variable} font-geist-mono antialiased`}>
         <ModeProvider defaultMode="realistic" storageKey="ui-mode">
-          <BackgroundLoader
-            placeholder={BACKGROUND_PLACEHOLDER}
-            optimized={BACKGROUND_OPTIMIZED}
-            className="min-h-screen text-foreground font-mono flex flex-col md:flex-row items-center justify-center p-2 md:p-8"
-          >
+          <BackgroundLoader placeholder={BACKGROUND_PLACEHOLDER} optimized={BACKGROUND_OPTIMIZED} className="min-h-screen text-foreground font-mono flex flex-col md:flex-row items-center justify-center p-2 md:p-8">
             <TVNoise opacity={1} intensity={0.2} speed={40} />
             <ModeWrapper>{children}</ModeWrapper>
           </BackgroundLoader>
           <ModeSwitcher />
         </ModeProvider>
+        <Analytics />
       </body>
     </html>
   );
